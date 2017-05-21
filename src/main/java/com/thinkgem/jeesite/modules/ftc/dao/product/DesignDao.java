19e85f694cdcfb16dadb2dc814db210d40571967
wangqingxiang <heavendarren@126.com>
2017-05-21 22:09:46 +0800

    修改设计和回复的界面

--- src/main/java/com/thinkgem/jeesite/modules/ftc/dao/product/DesignDao.java
+++ src/main/java/com/thinkgem/jeesite/modules/ftc/dao/product/DesignDao.java
@@ -10,7 +10,7 @@ import com.thinkgem.jeesite.modules.ftc.entity.product.Design;
 /**
  * 设计DAO接口
  * @author wangqingxiang
- * @version 2017-05-19
+ * @version 2017-05-21
  */
 @MyBatisDao
 public interface DesignDao extends CrudDao<Design> {
