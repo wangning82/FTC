4d3839d13244dfbd1241bd1ff36c7a6285c774d9
wangqingxiang <heavendarren@126.com>
2017-07-11 21:36:49 +0800

    测试支付

--- src/main/java/com/thinkgem/jeesite/modules/ftc/dto/product/ProductDto.java
+++ src/main/java/com/thinkgem/jeesite/modules/ftc/dto/product/ProductDto.java
@@ -10,86 +10,24 @@ import java.util.List;
 /**
  * Created by bingbing on 2017/6/14.
  */
-public class ProductDto extends BaseDto<ProductDto>{
-    private String  id;//id
-    private String did;
+public class ProductDto extends BaseDto<ProductDto> {
+    private String id;//id
+    private String did;//设计id
     private String name;//名称
     private String desc;//描述
     private Double price;//价格
-    private int  count;//库存
-    private String categoryId;//分类id
-
+    private String category;//分类id
     private boolean open;//是否开放
-    private BigDecimal designPrice;
+    private BigDecimal designPrice;//设计费
 
-    private String picImg;//产品主图
-
-    List<ProductImageDto> textures;//图片
-    List<ProductSpecDto> attrs;//规格
+    ProductSpecDto showAttibuteGroup;//默认的规格
+    List<ProductSpecDto> othersAttributeGroup;//规格列表
 
 
     private Integer priaseCount;//点赞数量
     private Integer favouritCount;//收藏数量
-    private boolean priased;
-    private boolean favourited;
-
-
-    public Integer getPriaseCount() {
-        return priaseCount;
-    }
-
-    public void setPriaseCount(Integer priaseCount) {
-        this.priaseCount = priaseCount;
-    }
-
-    public Integer getFavouritCount() {
-        return favouritCount;
-    }
-
-    public void setFavouritCount(Integer favouritCount) {
-        this.favouritCount = favouritCount;
-    }
-
-    public boolean isPriased() {
-        return priased;
-    }
-
-    public void setPriased(boolean priased) {
-        this.priased = priased;
-    }
-
-    public boolean isFavourited() {
-        return favourited;
-    }
-
-    public void setFavourited(boolean favourited) {
-        this.favourited = favourited;
-    }
-
-    public String getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(String categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getPicImg() {
-        return picImg;
-    }
-
-    public void setPicImg(String picImg) {
-        this.picImg = picImg;
-    }
-
-
-    public BigDecimal getDesignPrice() {
-        return designPrice;
-    }
-
-    public void setDesignPrice(BigDecimal designPrice) {
-        this.designPrice = designPrice;
-    }
+    private boolean priased;//点赞数量
+    private boolean favourited;//收藏数量
 
     public String getId() {
         return id;
@@ -131,12 +69,13 @@ public class ProductDto extends BaseDto<ProductDto>{
         this.price = price;
     }
 
-    public int getCount() {
-        return count;
+
+    public String getCategory() {
+        return category;
     }
 
-    public void setCount(int count) {
-        this.count = count;
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     public boolean isOpen() {
@@ -147,19 +86,62 @@ public class ProductDto extends BaseDto<ProductDto>{
         this.open = open;
     }
 
-    public List<ProductImageDto> getTextures() {
-        return textures;
+    public BigDecimal getDesignPrice() {
+        return designPrice;
     }
 
-    public void setTextures(List<ProductImageDto> textures) {
-        this.textures = textures;
+    public void setDesignPrice(BigDecimal designPrice) {
+        this.designPrice = designPrice;
     }
 
-    public List<ProductSpecDto> getAttrs() {
-        return attrs;
+    public ProductSpecDto getShowAttibuteGroup() {
+        return showAttibuteGroup;
     }
 
-    public void setAttrs(List<ProductSpecDto> attrs) {
-        this.attrs = attrs;
+    public void setShowAttibuteGroup(ProductSpecDto showAttibuteGroup) {
+        this.showAttibuteGroup = showAttibuteGroup;
+    }
+
+    public List<ProductSpecDto> getOthersAttributeGroup() {
+        return othersAttributeGroup;
+    }
+
+    public void setOthersAttributeGroup(List<ProductSpecDto> othersAttributeGroup) {
+        this.othersAttributeGroup = othersAttributeGroup;
+    }
+
+    public Integer getPriaseCount() {
+        return priaseCount;
+    }
+
+    public void setPriaseCount(Integer priaseCount) {
+        this.priaseCount = priaseCount;
+    }
+
+    public Integer getFavouritCount() {
+        return favouritCount;
+    }
+
+    public void setFavouritCount(Integer favouritCount) {
+        this.favouritCount = favouritCount;
+    }
+
+    public boolean isPriased() {
+        return priased;
+    }
+
+    public void setPriased(boolean priased) {
+        this.priased = priased;
+    }
+
+    public boolean isFavourited() {
+        return favourited;
+    }
+
+    public void setFavourited(boolean favourited) {
+        this.favourited = favourited;
     }
 }
+
+
+
